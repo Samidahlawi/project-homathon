3e89fa854a8a1af64423dd49bea0630ec34c485a
samidahlawi <o.t5@hotmail.com>
2020-04-28 01:38:08 +0300

    Updates

---
+++ precache-manifest.4ce6ca8a69b312ec4daac2f753967247.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cebbb43b82403acc989af8104da4951",
+    "url": "/easyPass/index.html"
+  },
+  {
+    "revision": "a1adf18c8ff702351384",
+    "url": "/easyPass/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3351ad9e745c0174b3f7",
+    "url": "/easyPass/static/css/main.9f597130.chunk.css"
+  },
+  {
+    "revision": "a1adf18c8ff702351384",
+    "url": "/easyPass/static/js/2.c480a947.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/easyPass/static/js/2.c480a947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3351ad9e745c0174b3f7",
+    "url": "/easyPass/static/js/main.a0751f44.chunk.js"
+  },
+  {
+    "revision": "2e6241a9c0cff5e8fd70",
+    "url": "/easyPass/static/js/runtime-main.9b6b7099.js"
+  },
+  {
+    "revision": "c793d8a4e644e6bfc67dda3cff21c139",
+    "url": "/easyPass/static/media/logo.c793d8a4.png"
+  },
+  {
+    "revision": "aa3891304a9dba915fcaa5df703ff771",
+    "url": "/easyPass/static/media/qrcode.aa389130.png"
+  }
+]);
